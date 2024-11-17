884577f2523e744083cfb882405ca5f6caff6988
Cristina Secu <secu.cristina@yahoo.com>
2024-11-17 15:17:06 -0500

    structured Roadmap page into static components; focused on semantic headings and hierarchical structure

--- src/features/roadmap/Roadmap.tsx
+++ src/features/roadmap/Roadmap.tsx
@@ -1,5 +1,47 @@
 function Roadmap(): React.JSX.Element {
-  return <h1>Roadmap</h1>;
+  return (
+    <>
+      <header className="roadmap_header">
+        <button>Go back</button>
+        <h1>Roadmap</h1>
+      </header>
+      <main className="roadmap_devStatusPhases">
+        <section className="roadmap_devStatusPhase">
+          <h2>Planned</h2>
+          <article>
+            <h3>Add Dark Theme</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              sit consectetur distinctio ipsam, in alias cupiditate corporis
+              natus ratione.
+            </p>
+          </article>
+        </section>
+        <section className="roadmap_devStatusPhase">
+          <h2>Planned</h2>
+          <article>
+            <h3>Add Dark Theme</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              sit consectetur distinctio ipsam, in alias cupiditate corporis
+              natus ratione.
+            </p>
+          </article>
+        </section>
+        <section className="roadmap_devStatusPhase">
+          <h2>Planned</h2>
+          <article>
+            <h3>Add Dark Theme</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat
+              sit consectetur distinctio ipsam, in alias cupiditate corporis
+              natus ratione.
+            </p>
+          </article>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Roadmap;
