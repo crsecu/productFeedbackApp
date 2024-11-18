26f0f4fa54cb8ab8550bd1e61c5a1ab2c265637d
Cristina Secu <secu.cristina@yahoo.com>
2024-11-18 11:24:40 -0500

    add temporary NavLink to navigate from Homepage to Feedback Board Page for easier development while Homepage remains under construction

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -1,5 +1,12 @@
+import { NavLink } from "react-router-dom";
+
 function Home(): React.JSX.Element {
-  return <h1>Homepage</h1>;
+  return (
+    <>
+      <h1>Homepage</h1>
+      <NavLink to="/feedbackBoard">Explore Product Feedback App</NavLink>
+    </>
+  );
 }
 
 export default Home;
