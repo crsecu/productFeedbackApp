b54247d476c8a17ffca7144a5f5e9fe789e34049
Cristina Secu <secu.cristina@yahoo.com>
2025-02-15 22:27:10 -0500

    save

--- src/features/feedback/CreateFeedbackBest.tsx
+++ src/features/feedback/CreateFeedbackBest.tsx
@@ -6,15 +6,17 @@ import {
 } from "react-router-dom";
 import FeedbackFormBest from "./FeedbackFormBest";
 import { FeedbackFormData } from "../../types/feedback.types";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useDispatch } from "react-redux";
 import { showModal } from "../../store/slices/modalSlice";
+import { hasFormChanged } from "../../utils/helpers";
 
 const initialFormState = {
   title: "",
   category: "feature",
   description: "",
 };
+
 function CreateFeedbackBest(): React.JSX.Element {
   const dispatch = useDispatch();
   const formData = useActionData() as FeedbackFormData;
@@ -24,21 +26,10 @@ function CreateFeedbackBest(): React.JSX.Element {
   const isSubmitting = navigation.state === "submitting";
 
   const prevPage = useRef(state?.from);
+  const [isFormDirty, setIsFormDirty] = useState(false);
 
-  function handleFormDismiss(e) {
-    const formData = new FormData(e.target.form);
-
-    const data = Object.fromEntries(formData);
-    const hasFormChanged =
-      JSON.stringify(initialFormState) !== JSON.stringify(data);
-
-    console.log(
-      "is data the same",
-      JSON.stringify(initialFormState) === JSON.stringify(data),
-      data
-    );
-
-    if (!hasFormChanged) {
+  function handleCancel(e) {
+    if (!isFormDirty) {
       navigate(prevPage.current);
     } else {
       dispatch(
@@ -50,15 +41,25 @@ function CreateFeedbackBest(): React.JSX.Element {
     }
   }
 
+  function handleFormChange(e: { currentTarget: HTMLFormElement | undefined }) {
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const isDirty = hasFormChanged(initialFormState, data);
+
+    if (isDirty !== isFormDirty) setIsFormDirty(isDirty);
+  }
+
   return (
     <FeedbackFormBest
+      handleChange={handleFormChange}
       method="post"
       footer={
         <>
-          <button disabled={isSubmitting}>
+          <button disabled={isSubmitting || isFormDirty === false}>
             {isSubmitting ? "Submitting..." : "Add Feedback"}
           </button>
-          <button type="button" onClick={(e) => handleFormDismiss(e)}>
+          <button type="button" onClick={(e) => handleCancel(e)}>
             Cancel
           </button>
         </>
