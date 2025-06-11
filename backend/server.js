41a7a2aad98d0cb1f004d81075d60813a10f855f
Cristina Secu <secu.cristina@yahoo.com>
2025-06-11 15:33:46 -0400

    fix CORS issue by using cors middleware in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,15 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
-const middlewares = jsonServer.defaults();
 
 // Load the JSON manually and use in-memory data
 const data = require("./db.json");
 const router = jsonServer.router(data);
 
-//CORS headers
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
-  );
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+const middlewares = jsonServer.defaults();
 
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
