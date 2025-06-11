fcf68f98a89e392075cd6044110e92f458541a11
Cristina Secu <secu.cristina@yahoo.com>
2025-06-11 12:20:33 -0400

    split project into frontend and backend folders for deployment

--- frontend/src/store/slices/userSlice.ts
+++ frontend/src/store/slices/userSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AppState } from "../../store/store";
+import { AppState } from "../store";
 import { User } from "../../features/user/user.types";
 
 interface UserState {
