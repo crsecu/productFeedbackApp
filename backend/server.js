2aae9d9fda17a3c173219a2ed97debb90eed0b07
Cristina Secu <secu.cristina@yahoo.com>
2025-06-11 15:26:41 -0400

    add OPTIONS to CORS header

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ const router = jsonServer.router(data);
 //CORS headers
 server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
